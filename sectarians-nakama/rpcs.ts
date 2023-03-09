3bd85a70017f4c890f18efe04108e4aaa592f45b
Yaroslav <jaroslav-nikiti@rambler.ru>
2023-03-09 21:32:11 +0500

    Update rpcs.ts

--- sectarians-nakama/rpcs.ts
+++ sectarians-nakama/rpcs.ts
@@ -4,7 +4,7 @@ let joinOrCreateMatch: nkruntime.RpcFunction = function (context: nkruntime.Cont
     const MatchesLimit = 10;
     const isAuthoritative = true;
     var label: MatchLabel = { open: true }
-    matches = nakama.matchList(MatchesLimit, isAuthoritative, JSON.stringify(label), MinimumPlayers, 1); //MaxPlayers - 1
+    matches = nakama.matchList(MatchesLimit, isAuthoritative, JSON.stringify(label), MinimumPlayers, MaxTestPlayers - 1); //MaxPlayers - 1
     if (matches.length > 0)
         return matches[0].matchId;
 
