d3093793e008d4c2bde4c8065820dbad0ed7b31b
Yaroslav <jaroslav-nikiti@rambler.ru>
2023-03-11 22:13:41 +0500

    Declare queriesToApi as global test 2

--- sectarians-nakama/match_handler.ts
+++ sectarians-nakama/match_handler.ts
@@ -1,12 +1,9 @@
 const PathToIdsJson: string = './ids.json';
-
-declare global {
-    var queriesToApi: boolean = false;
-}
+declare var QueriesToApi: boolean = false;
 
 let matchInit: nkruntime.MatchInitFunction = function (context: nkruntime.Context, logger: nkruntime.Logger, nakama: nkruntime.Nakama, params: { [key: string]: string })
 {
-    window.queriesToApi = false;
+    QueriesToApi = false;
 
     type Match = {
         matchId: number
@@ -162,7 +159,7 @@ function matchLoopLobby(gameState: GameState, nakama: nkruntime.Nakama, dispatch
     {
         dispatcher.matchLabelUpdate(JSON.stringify({ open: false }));
         gameState.countdown--;
-        if (!window.queriesToApi) {
+        if (!QueriesToApi) {
             let startBody = JSON.stringify({
                 data: {'game': gameState.matchId, 'count': 5, 'amount': PlayerPayment, 'currency': 'USDR'},
                 sign: ''
@@ -179,7 +176,7 @@ function matchLoopLobby(gameState: GameState, nakama: nkruntime.Nakama, dispatch
                 post_api('http://localhost:8080/api/contract/sectarians/buy', buyBody, logger);
                 testCount++;
             }
-            window.queriesToApi = true;
+            QueriesToApi = true;
         }
 
         if (gameState.countdown % 10 == 0)
