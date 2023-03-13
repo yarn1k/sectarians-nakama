d01c719c07c7cc82888ca6c8d8179ee2fcdb4a33
Yaroslav <jaroslav-nikiti@rambler.ru>
2023-03-13 22:25:08 +0500

    Update match_handler.ts

--- sectarians-nakama/match_handler.ts
+++ sectarians-nakama/match_handler.ts
@@ -181,9 +181,10 @@ function matchLoopLobby(gameState: GameState, nakama: nkruntime.Nakama, dispatch
             for (let payment of payments) {
                 if (payment.status == 1) {
                     let player = getPlayerByWalletId(gameState.players, nakama, payment.buyer);
-                    logger.info("player="+player.presence.userId);
-                    if (player != null)
+                    if (player != null) {
+                        logger.info("player="+player.presence.userId);
                         playerPaid(gameState, player);
+                    }
                 } else if (payment.status == 2) {
                     dispatcher.broadcastMessage(OperationCode.CancelMatch, null);
                     gameState.endMatch = true;
